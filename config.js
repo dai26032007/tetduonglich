1d53101ac180707f335ec2b718c3a4c615d35936
dai26032007 <dai26032007@gmail.com>
2024-12-31 19:59:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🌺Chào cậu!🌺',
+    introDesc: `Bình thường thì chỉ có Tết âm lịch thì mới có lì xì. Nhưng vì đây là Tết dương lịch cuối cùng chúng ta học với nhau nên tôi sẽ lì xì bạn`,
+    btnIntro: 'Oki',
+    title: 'Bạn có mún nhận lì xì từ Đại hông?🙆‍♂️',
+    btnYes: 'Không thích',
+    btnNo: 'Có, nhận chứ😁',
+    mess: 'Buồn zậy😢 Nếu cậu không thích nhận thì thoi zậy, tớ cũng hông ép😕',
+    messDesc: 'Happy newyear nha!🎆'
+}
