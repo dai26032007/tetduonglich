0439651bf2731d19edb86e383e6cf23445376179
dai26032007 <dai26032007@gmail.com>
2024-12-31 20:02:54 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+    // process bar
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function init(){
+    $('#title').text(CONFIG.title)
+    $('#yes').text(CONFIG.btnYes)
+    $('#no').text(CONFIG.btnNo)
+}
+
+function firstQuestion(){
+    Swal.fire({
+        title: CONFIG.introTitle,
+        text: CONFIG.introDesc,
+
+        // 2. Ảnh ở câu hỏi đầu tiên
+        imageUrl: 'img/cuteCat.png',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+        confirmButtonText: CONFIG.btnIntro
+      }).then(function(){
+        var audio = new Audio('sound/nhactet.mp3');
+        audio.play();
+        $('.content').show(200);
+      })
+}
+
+ // switch button position
+ function switchButton() {
+    var leftNo = $('#no').css("left");
+    var topNO = $('#no').css("top");
+    var leftY = $('#yes').css("left");
+    var topY = $('#yes').css("top");
+    $('#no').css("left", leftY);
+    $('#no').css("top", topY);
+    $('#yes').css("left", leftNo);
+    $('#yes').css("top", topNO);
+}
+
+function moveButton() {
+    var audio = new Audio('sound/Swish1.mp3');
+    audio.play();
+    var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
+    var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
+    var left = x + 'px';
+    var top = y + 'px';
+    $('#no').css("left", left);
+    $('#no').css("top", top);
+}
+
+init()
+
+var n = 0;
+$('#no').mousemove(function() {
+    var audio = new Audio('sound/duck.mp3'); // Thêm âm thanh khi nhấn nút từ chối
+    audio.play();
+    if (n < 1)
+        switchButton();
+    if (n > 1)
+        moveButton();
+    n++;
+});
+$('#no').click(() => {
+    var audio = new Audio('sound/duck.mp3'); // Thêm âm thanh khi nhấn nút từ chối
+    audio.play();
+    if (screen.width>=900)
+        switchButton();
+})
+
+// generate text in input
+function textGenerate() {
+    var n = "";
+    var text = " " + CONFIG.reply;
+    var a = Array.from(text);
+    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+        for (let i = 1; i <= count; i++) {
+            n = n + a[i];
+            if (i == text.length + 1) {
+                $('#txtReason').val("");
+                n = "";
+                break;
+            }
+        }
+    }
+    $('#txtReason').val(n);
+    setTimeout("textGenerate()", 1);
+}
+
+// show popup
+$('#yes').click(function() {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
+    Swal.fire({
+        width: 900,
+        background: '#fff url("img/iput-bg.jpg")',
+        title: CONFIG.mess,
+        text: CONFIG.messDesc,
+        showConfirmButton: false, // Ẩn nút xác nhận
+    });
+});
